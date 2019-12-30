3342b47b9f85532c2528a89293bb5fdc875d1383
ahlev <ohahlev@gmail.com>
2019-12-30 22:33:25 +0700

    add version into verbose name

--- contact_us/apps.py
+++ contact_us/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.html import format_html
+from . import __version__ as VERSION
 
 class ContactUsConfig(AppConfig):
     name = 'contact_us'
-    verbose_name = 'Contact Us Management'
+    verbose_name = format_html("Contact Us Management <span class='version'>{}</span>", VERSION)
 
