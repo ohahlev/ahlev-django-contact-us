507c7227dfceba3fa78967096023493d77a9ff26
ahlev <ohahlev@gmail.com>
2020-01-21 00:33:50 +0700

    remove style from version label

--- contact_us/apps.py
+++ contact_us/apps.py
@@ -4,5 +4,5 @@ from . import __version__ as VERSION
 
 class ContactUsConfig(AppConfig):
     name = 'contact_us'
-    verbose_name = format_html("Contact Us Management <span class='version'>{}</span>", VERSION)
+    verbose_name = format_html("Contact Us Management {}", VERSION)
 
